18ce38b2636395e092db27d509da499ffe345889
Kian <villenokian>
2020-08-07 22:41:45 +0800

    ambot

--- index.js
+++ index.js
@@ -34,10 +34,10 @@ const PORT = process.env.PORT || 5000;
 // app.use('/search', searchRoute);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('my-app/build'));
   const path = require('path');
   app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    res.sendFile(path.resolve(__dirname,'my-app','build','index.html'));
   });
 }
 
